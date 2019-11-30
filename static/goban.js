a0d20af31fdb8f0d96ef84941e886eb935868779
Joshua Sterner <joshua.s.sterner@gmail.com>
2019-11-30 00:40:16 -0800

    Added grid panning to game page with mouse and touch support. Disabled user-scaling on game page so that pinch to zoom gesture can control grid size (not implemented yet).

--- static/goban.js
+++ static/goban.js
@@ -1,48 +1,116 @@
-const Goban = function(canvas) {
-
-    const ctx = canvas.getContext('2d');
-
-    const grid_ratio = 22/23.7;
-
-    function draw_grid(grid_width, offset) {
-
-        const grid_height = grid_width / grid_ratio;
-
-        let x_offset = offset.x % grid_width;
-        let y_offset = offset.y % grid_height;
-
-        for (let x = x_offset; x < canvas.width; x += grid_width) {
-            ctx.beginPath();
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = 'black';
-            ctx.moveTo(x, 0);
-            ctx.lineTo(x, canvas.height);
-            ctx.stroke();
-        }
-        for (let y = y_offset; y < canvas.height; y += grid_height) {
-            ctx.beginPath();
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = 'black';
-            ctx.moveTo(0, y);
-            ctx.lineTo(canvas.width, y);
-            ctx.stroke();
-        }
+class Goban {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.grid_ratio = 22/23.7;
+        this.grid_width = 40;
+        this.offset = {x:16, y:16};
+        this.panning = false;
+        this.panning_from = {x:0, y:0};
+        this.panning_touch_id = null;
+        canvas.addEventListener('mousedown', (e) => {
+            this.panning = true;
+            this.panning_from.x = e.clientX;
+            this.panning_from.y = e.clientY;
+        });
+        canvas.addEventListener('mouseup', (e) => {
+            this.panning = false;
+        });
+        canvas.addEventListener('mouseout', (e) => {
+            this.panning = false;
+        });
+        canvas.addEventListener('mousemove', (e) => {
+            if (!this.panning) {
+                return;
+            }
+            const delta_x = e.clientX - this.panning_from.x;
+            const delta_y = e.clientY - this.panning_from.y;
+            this.offset.x += delta_x;
+            this.offset.y += delta_y;
+            this.panning_from.x = e.clientX;
+            this.panning_from.y = e.clientY;
+            window.requestAnimationFrame(() => this.draw());
+        });
+        canvas.addEventListener('touchstart', (e) => {
+            if (e.touches.length != 1) {
+                this.panning = false;
+                return;
+            }
+            this.panning_from.x = e.touches[0].clientX;
+            this.panning_from.y = e.touches[0].clientY;
+            this.panning_touch_id = e.touches[0].identifier;
+            this.panning = true;
+        });
+        canvas.addEventListener('touchend', (e) => {
+            for (let i = 0; i < e.touches.length; i++) {
+                const touch = e.touches[i];
+                if (touch.identifier == this.panning_touch_id) {
+                    this.panning = false;
+                    this.panning_touch_id = null;
+                }
+            }
+        });
+        canvas.addEventListener('touchmove', (e) => {
+            if (e.touches.length != 1 || e.touches[0].identifier != this.panning_touch_id) {
+                this.panning = false;
+                this.panning_touch_id = null;
+                return;
+            }
+            const delta_x = e.touches[0].clientX - this.panning_from.x;
+            const delta_y = e.touches[0].clientY - this.panning_from.y;
+            this.offset.x += delta_x;
+            this.offset.y += delta_y;
+            this.panning_from.x = e.touches[0].clientX;
+            this.panning_from.y = e.touches[0].clientY;
+            window.requestAnimationFrame(() => this.draw());
+        });
     }
+
+
+    foo() {
+        console.log(this.canvas);
+    }
+
     
-    function draw() {
-        draw_grid(40, {x:16, y:16});
+    draw_grid() {
+        const grid_width = this.grid_width;
+        const grid_height = grid_width / this.grid_ratio;
+
+        const x_offset = this.offset.x % grid_width;
+        const y_offset = this.offset.y % grid_height;
+
+        for (let x = x_offset; x < this.canvas.width; x += grid_width) {
+            this.ctx.beginPath();
+            this.ctx.lineWidth = 2;
+            this.ctx.strokeStyle = 'black';
+            this.ctx.moveTo(x, 0);
+            this.ctx.lineTo(x, this.canvas.height);
+            this.ctx.stroke();
+        }
+        for (let y = y_offset; y < this.canvas.height; y += grid_height) {
+            this.ctx.beginPath();
+            this.ctx.lineWidth = 2;
+            this.ctx.strokeStyle = 'black';
+            this.ctx.moveTo(0, y);
+            this.ctx.lineTo(this.canvas.width, y);
+            this.ctx.stroke();
+        }
     }
 
-    function resize() {
-        canvas.width = canvas.clientWidth;
-        canvas.height = canvas.clientHeight;
-        window.requestAnimationFrame(draw);
+    
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.draw_grid();
     }
 
-    this.resize = resize;
+    resize() {
+        this.canvas.width = this.canvas.clientWidth;
+        this.canvas.height = this.canvas.clientHeight;
+        window.requestAnimationFrame(() => this.draw());
+    }
+
 }
-
 const goban = new Goban(document.getElementById('goban'));
 
-window.addEventListener('resize', goban.resize);
-window.addEventListener('load', goban.resize);
+window.addEventListener('resize', () => goban.resize());
+window.addEventListener('load', () => goban.resize());
