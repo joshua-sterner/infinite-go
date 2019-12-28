7b916734510f0caa2417633772f4dc89de6ef90c
Joshua Sterner <joshua.s.sterner@gmail.com>
2019-12-28 00:27:01 -0800

    Started planning out tests for tapping/clicking on goban. Prepended underscore to "private" methods/fields of frontent Goban class.

--- static/goban.js
+++ static/goban.js
@@ -1,160 +1,170 @@
 class Goban {
+    //TODO prefix private stuff w/ underscore
     constructor(canvas) {
-        this.canvas = canvas;
-        this.ctx = canvas.getContext('2d');
-        this.grid_ratio = 22/23.7;
+        this._canvas = canvas;
+        this._ctx = canvas.getContext('2d');
+        this._grid_ratio = 22/23.7;
         //TODO use grid height instead
-        this.grid_width = 40;
-        this.offset = {x:60, y:60};
-        this.panning_from = {x:0, y:0};
-        this.panning_touch_id = null;
+        this._grid_width = 40;
+        this._panning = false;
+        this._panning_from = {x:0, y:0};
+        this._panning_touch_id = null;
         this.stones = [];
         this.stone_color = 'white';
         this.offset = {x: 0, y: 0};
-        this.initial_touch_position = {x: -1024, y: -1024};
+        this._initial_touch_position = {x: -1024, y: -1024};
 
-        canvas.addEventListener('mousedown', (e) => this.handle_press(e.clientX, e.clientY));
-        canvas.addEventListener('mouseup', (e) => this.handle_release(e.clientX, e.clientY));
-        canvas.addEventListener('mousemove', (e) => this.handle_move(e.clientX, e.clientY));
-        canvas.addEventListener('mouseout', (e) => this.handle_out());
+        canvas.addEventListener('mousedown', (e) => this._handle_press(e.clientX, e.clientY));
+        canvas.addEventListener('mouseup', (e) => this._handle_release(e.clientX, e.clientY));
+        canvas.addEventListener('mousemove', (e) => this._handle_move(e.clientX, e.clientY));
+        canvas.addEventListener('mouseout', (e) => this._handle_out());
 
         canvas.addEventListener('touchstart', (e) => {
             if (e.touches.length != 1) {
-                this.handle_out();
+                this._handle_out();
                 return;
             }
-            this.handle_press(e.touches[0].clientX, e.touches[0].clientY);
+            this._handle_press(e.touches[0].clientX, e.touches[0].clientY);
         });
         canvas.addEventListener('touchend', (e) => {
             if (e.touches.length == 0 && e.changedTouches.length == 1) {
-                this.handle_release(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
+                this._handle_release(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
             }
         });
         canvas.addEventListener('touchmove', (e) => {
             if (e.touches.length == 1) {
-                this.handle_move(e.touches[0].clientX, e.touches[0].clientY);
+                this._handle_move(e.touches[0].clientX, e.touches[0].clientY);
             }
         });
         canvas.addEventListener('touchcancel', (e) => {
             if (e.touches.length == 1) {
-                this.handle_out();
+                this._handle_out();
             }
         });
     }
 
-    handle_press(x, y) {
-        this.panning = true;
-        this.panning_from.x = x;
-        this.panning_from.y = y;
-        this.initial_touch_position = {x: x, y: y};
+    resize() {
+        //TODO window.devicePixelRatio || 1
+        this._canvas.width = this._canvas.clientWidth;
+        this._canvas.height = this._canvas.clientHeight;
+        window.requestAnimationFrame(() => this._draw());
     }
 
-    handle_release(x, y) {
-        this.panning = false;
-        if (this.is_click(x, y)) {
-            this.grid_click_release(x, y);
+    _handle_press(x, y) {
+        this._panning = true;
+        this._panning_from.x = x;
+        this._panning_from.y = y;
+        this._initial_touch_position = {x: x, y: y};
+    }
+
+    _handle_release(x, y) {
+        this._panning = false;
+        if (this._is_click(x, y)) {
+            this._grid_click_release(x, y);
         }
     }
 
-    handle_move(x, y) {
-        if (!this.panning) {
+    _handle_move(x, y) {
+        if (!this._panning) {
             return;
         }
-        const delta_x = x - this.panning_from.x;
-        const delta_y = y - this.panning_from.y;
+        const delta_x = x - this._panning_from.x;
+        const delta_y = y - this._panning_from.y;
         this.offset.x += delta_x;
         this.offset.y += delta_y;
-        this.panning_from.x = x; 
-        this.panning_from.y = y;
-        window.requestAnimationFrame(() => this.draw());
+        this._panning_from.x = x; 
+        this._panning_from.y = y;
+        window.requestAnimationFrame(() => this._draw());
     }
 
-    handle_out() {
-        this.panning = false;
+    _handle_out() {
+        this._panning = false;
     }
 
-    is_click(x, y) {
-        const threshold = 8;
-        const delta_x = Math.abs(x - this.initial_touch_position.x);
-        const delta_y = Math.abs(y - this.initial_touch_position.y);
-        return delta_x < threshold && delta_y < threshold;
-    }
-
-    grid_click_release(x, y) {
-        const out_pos = this.to_grid_position(x, y);
+    _grid_click_release(x, y) {
+        const out_pos = this._to_grid_position(x, y);
         if (!this.unconfirmed_stone) {
-            this.place_unconfirmed_stone(this.stone_color, out_pos);
+            this._place_unconfirmed_stone(this.stone_color, out_pos);
         } else {
             if (this.unconfirmed_stone.position.x == out_pos.x &&
                 this.unconfirmed_stone.position.y == out_pos.y) {
-                this.confirm_stone_placement();
+                this._confirm_stone_placement();
             } else {
-                this.place_unconfirmed_stone(this.stone_color, out_pos);
+                this._place_unconfirmed_stone(this.stone_color, out_pos);
             }
         }
-        window.requestAnimationFrame(() => this.draw());
+        window.requestAnimationFrame(() => this._draw());
     }
 
-    to_grid_position(x, y) {
-        x = Math.floor((x - this.offset.x + (0.5*this.grid_width))/this.grid_width);
-        y = Math.floor((y - this.offset.y + 0.5*(this.grid_width/this.grid_ratio))/(this.grid_width/this.grid_ratio));
+    _is_click(x, y) {
+        const threshold = 8;
+        const delta_x = Math.abs(x - this._initial_touch_position.x);
+        const delta_y = Math.abs(y - this._initial_touch_position.y);
+        return delta_x < threshold && delta_y < threshold;
+    }
+
+
+
+    _to_grid_position(x, y) {
+        x = Math.floor((x - this.offset.x + (0.5*this._grid_width))/this._grid_width);
+        y = Math.floor((y - this.offset.y + 0.5*(this._grid_width/this._grid_ratio))/(this._grid_width/this._grid_ratio));
         return {x:x, y:y};
     }
 
-    draw_grid() {
-        const grid_width = this.grid_width;
-        const grid_height = grid_width / this.grid_ratio;
+    _draw_grid() {
+        const grid_width = this._grid_width;
+        const grid_height = grid_width / this._grid_ratio;
 
         const x_offset = this.offset.x % grid_width;
         const y_offset = this.offset.y % grid_height;
 
-        for (let x = x_offset; x < this.canvas.width; x += grid_width) {
-            this.ctx.beginPath();
-            this.ctx.lineWidth = 2;
-            this.ctx.strokeStyle = 'black';
-            this.ctx.moveTo(x, 0);
-            this.ctx.lineTo(x, this.canvas.height);
-            this.ctx.stroke();
+        for (let x = x_offset; x < this._canvas.width; x += grid_width) {
+            this._ctx.beginPath();
+            this._ctx.lineWidth = 2;
+            this._ctx.strokeStyle = 'black';
+            this._ctx.moveTo(x, 0);
+            this._ctx.lineTo(x, this._canvas.height);
+            this._ctx.stroke();
         }
-        for (let y = y_offset; y < this.canvas.height; y += grid_height) {
-            this.ctx.beginPath();
-            this.ctx.lineWidth = 2;
-            this.ctx.strokeStyle = 'black';
-            this.ctx.moveTo(0, y);
-            this.ctx.lineTo(this.canvas.width, y);
-            this.ctx.stroke();
+        for (let y = y_offset; y < this._canvas.height; y += grid_height) {
+            this._ctx.beginPath();
+            this._ctx.lineWidth = 2;
+            this._ctx.strokeStyle = 'black';
+            this._ctx.moveTo(0, y);
+            this._ctx.lineTo(this._canvas.width, y);
+            this._ctx.stroke();
         }
     }
 
-    draw_stone(color, pos, unconfirmed) {
-        const x = pos.x * this.grid_width + this.offset.x;
-        const y = pos.y * (this.grid_width/this.grid_ratio) + this.offset.y;
-        this.ctx.beginPath();
-        this.ctx.lineWidth = 2;
-        this.ctx.strokeStyle = 'black';
-        this.ctx.fillStyle = color;
+    _draw_stone(color, pos, unconfirmed) {
+        const x = pos.x * this._grid_width + this.offset.x;
+        const y = pos.y * (this._grid_width/this._grid_ratio) + this.offset.y;
+        this._ctx.beginPath();
+        this._ctx.lineWidth = 2;
+        this._ctx.strokeStyle = 'black';
+        this._ctx.fillStyle = color;
         //TODO stone width
         if (unconfirmed) {
-            this.ctx.strokeStyle = color;
-            this.ctx.fillStyle = 'rgba(0,0,0,0)';
+            this._ctx.strokeStyle = color;
+            this._ctx.fillStyle = 'rgba(0,0,0,0)';
         }
-        this.ctx.arc(x, y, this.grid_width*0.5-3, 0, 2*Math.PI);
-        this.ctx.fill();
-        this.ctx.stroke();
+        this._ctx.arc(x, y, this._grid_width*0.5-3, 0, 2*Math.PI);
+        this._ctx.fill();
+        this._ctx.stroke();
     }
 
-    draw() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.draw_grid();
+    _draw() {
+        this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+        this._draw_grid();
         this.stones.forEach((stone) => {
-            this.draw_stone(stone.color, stone.position);
+            this._draw_stone(stone.color, stone.position);
         });
         if (this.unconfirmed_stone) {
-            this.draw_stone(this.unconfirmed_stone.color, this.unconfirmed_stone.position, true);
+            this._draw_stone(this.unconfirmed_stone.color, this.unconfirmed_stone.position, true);
         }
     }
 
-    point_empty(pos) {
+    _point_empty(pos) {
         for (let i = 0; i < this.stones.length; i++) {
             const stone = this.stones[i];
             if (pos.x == stone.position.x && pos.y == stone.position.y) {
@@ -164,22 +174,15 @@ class Goban {
         return true;
     }
 
-    place_unconfirmed_stone(color, pos) {
-        if (this.point_empty(pos)) {
+    _place_unconfirmed_stone(color, pos) {
+        if (this._point_empty(pos)) {
             this.unconfirmed_stone = {color: color, position: pos};
         }
     }
 
-    confirm_stone_placement() {
+    _confirm_stone_placement() {
         this.stones.push(this.unconfirmed_stone);
         this.unconfirmed_stone = null;
-    }
-
-    resize() {
-        //TODO window.devicePixelRatio || 1
-        this.canvas.width = this.canvas.clientWidth;
-        this.canvas.height = this.canvas.clientHeight;
-        window.requestAnimationFrame(() => this.draw());
     }
 
 }
--- test/goban.test.js
+++ test/goban.test.js
@@ -67,8 +67,8 @@ describe('Goban', () => {
             });
         });
         it('sets initial_touch_position to -1024,-1024 (far offscreen value)', () => {
-            assert.equal(goban.initial_touch_position.x, -1024);
-            assert.equal(goban.initial_touch_position.y, -1024);
+            assert.equal(goban._initial_touch_position.x, -1024);
+            assert.equal(goban._initial_touch_position.y, -1024);
         });
         it('sets initial offset to 0,0', () => {
             assert(goban.offset);
@@ -80,66 +80,66 @@ describe('Goban', () => {
     describe('Event Listeners', () => {
         beforeEach(() => {
 
-            // set up handle_press spy
-            goban.handle_press_calls = [];
-            const handle_press = goban.handle_press;
-            goban.handle_press = function(x, y) {
-                goban.handle_press_calls.push([x, y]);
-                handle_press.call(goban, x, y);
+            // set up _handle_press spy
+            goban._handle_press_calls = [];
+            const _handle_press = goban._handle_press;
+            goban._handle_press = function(x, y) {
+                goban._handle_press_calls.push([x, y]);
+                _handle_press.call(goban, x, y);
             };
 
-            // set up handle_release spy
-            goban.handle_release_calls = [];
-            const handle_release = goban.handle_release;
-            goban.handle_release = function(x, y) {
-                goban.handle_release_calls.push([x, y]);
-                handle_release.call(goban, x, y);
+            // set up _handle_release spy
+            goban._handle_release_calls = [];
+            const _handle_release = goban._handle_release;
+            goban._handle_release = function(x, y) {
+                goban._handle_release_calls.push([x, y]);
+                _handle_release.call(goban, x, y);
             };
 
-            // set up handle_move spy
-            goban.handle_move_calls = [];
-            const handle_move = goban.handle_move;
-            goban.handle_move = function(x, y) {
-                goban.handle_move_calls.push([x, y]);
-                handle_move.call(goban, x, y);
+            // set up _handle_move spy
+            goban._handle_move_calls = [];
+            const _handle_move = goban._handle_move;
+            goban._handle_move = function(x, y) {
+                goban._handle_move_calls.push([x, y]);
+                _handle_move.call(goban, x, y);
             };
 
-            // set up handle_out spy
-            goban.handle_out_calls = 0;
-            const handle_out = goban.handle_out;
-            goban.handle_out = function() {
-                goban.handle_out_calls++;
-                handle_out.call(goban);
+            // set up _handle_out spy
+            goban._handle_out_calls = 0;
+            const _handle_out = goban._handle_out;
+            goban._handle_out = function() {
+                goban._handle_out_calls++;
+                _handle_out.call(goban);
             };
         });
         describe('mousedown', () => {
-            it('calls handle_press with correct coordinates', () => {
+            it('calls _handle_press with correct coordinates', () => {
                 canvas.event_listeners['mousedown'][0]({clientX: 123, clientY: 456});
-                assert.equal(goban.handle_press_calls.length, 1);
-                assert.equal(goban.handle_press_calls[0][0], 123);
-                assert.equal(goban.handle_press_calls[0][1], 456);
+                assert.equal(goban._handle_press_calls.length, 1);
+                assert.equal(goban._handle_press_calls[0][0], 123);
+                assert.equal(goban._handle_press_calls[0][1], 456);
             });
         });
         describe('mouseup', () => {
-            it('calls handle_release with correct coordinates', () => {
+            it('calls _handle_release with correct coordinates', () => {
                 canvas.event_listeners['mouseup'][0]({clientX: 123, clientY: 456});
-                assert.equal(goban.handle_release_calls.length, 1);
-                assert.equal(goban.handle_release_calls[0][0], 123);
-                assert.equal(goban.handle_release_calls[0][1], 456);
+                assert.equal(goban._handle_release_calls.length, 1);
+                assert.equal(goban._handle_release_calls[0][0], 123);
+                assert.equal(goban._handle_release_calls[0][1], 456);
             });
         });
         describe('mousemove', () => {
-            it('calls handle_move with correct coordinates', () => {
+            it('calls _handle_move with correct coordinates', () => {
                 canvas.event_listeners['mousemove'][0]({clientX: 123, clientY: 456});
-                assert.equal(goban.handle_move_calls.length, 1);
-                assert.equal(goban.handle_move_calls[0][0], 123);
-                assert.equal(goban.handle_move_calls[0][1], 456);
+                assert.equal(goban._handle_move_calls.length, 1);
+                assert.equal(goban._handle_move_calls[0][0], 123);
+                assert.equal(goban._handle_move_calls[0][1], 456);
             });
         });
         describe('mouseout', () => {
-            it('calls handle_out', () => {
+            it('calls _handle_out', () => {
                 canvas.event_listeners['mouseout'][0]();
-                assert.equal(goban.handle_out_calls, 1);
+                assert.equal(goban._handle_out_calls, 1);
             });
         });
         describe('touch events', () => {
@@ -148,55 +148,55 @@ describe('Goban', () => {
             const single_touch_event = {touches: [touch]};
             const double_touch_event = {touches: [touch, touch2]};
             describe('touchstart', () => {
-                it('calls handle_press with correct coordinates when touches.length == 1', () => {
+                it('calls _handle_press with correct coordinates when touches.length == 1', () => {
                     canvas.event_listeners['touchstart'][0](single_touch_event);
-                    assert.equal(goban.handle_press_calls.length, 1);
-                    assert.equal(goban.handle_press_calls[0][0], 123);
-                    assert.equal(goban.handle_press_calls[0][1], 456);
+                    assert.equal(goban._handle_press_calls.length, 1);
+                    assert.equal(goban._handle_press_calls[0][0], 123);
+                    assert.equal(goban._handle_press_calls[0][1], 456);
                 });
-                it('calls handle_out when touches.length != 1', () => {
+                it('calls _handle_out when touches.length != 1', () => {
                     canvas.event_listeners['touchstart'][0](double_touch_event);
-                    assert.equal(goban.handle_out_calls, 1);
+                    assert.equal(goban._handle_out_calls, 1);
                 });
-                it('doesn\'t call handle_press when touches.length != 1', () => {
+                it('doesn\'t call _handle_press when touches.length != 1', () => {
                     canvas.event_listeners['touchstart'][0](double_touch_event);
-                    assert.equal(goban.handle_press_calls.length, 0);
+                    assert.equal(goban._handle_press_calls.length, 0);
                 });
             });
             describe('touchend', () => {
-                it('calls handle_release with correct coordinates when single touch ends', () => {
+                it('calls _handle_release with correct coordinates when single touch ends', () => {
                     const touch_event = {touches: [], changedTouches: [touch]};
                     canvas.event_listeners['touchend'][0](touch_event);
-                    assert.equal(goban.handle_release_calls.length, 1);
-                    assert.equal(goban.handle_release_calls[0][0], 123);
-                    assert.equal(goban.handle_release_calls[0][1], 456);
+                    assert.equal(goban._handle_release_calls.length, 1);
+                    assert.equal(goban._handle_release_calls[0][0], 123);
+                    assert.equal(goban._handle_release_calls[0][1], 456);
                 });
-                it('doesn\'t call handle_release when one of two touches end', () => {
+                it('doesn\'t call _handle_release when one of two touches end', () => {
                     const touch_event = {touches: [touch, touch2], changedTouches: [touch]};
                     canvas.event_listeners['touchend'][0](touch_event);
-                    assert.equal(goban.handle_release_calls.length, 0);
+                    assert.equal(goban._handle_release_calls.length, 0);
                 });
             });
             describe('touchmove', () => {
-                it('calls handle_move with correct coordinates when touches.length == 1', () => {
+                it('calls _handle_move with correct coordinates when touches.length == 1', () => {
                     canvas.event_listeners['touchmove'][0](single_touch_event);
-                    assert.equal(goban.handle_move_calls.length, 1);
-                    assert.equal(goban.handle_move_calls[0][0], 123);
-                    assert.equal(goban.handle_move_calls[0][1], 456);
+                    assert.equal(goban._handle_move_calls.length, 1);
+                    assert.equal(goban._handle_move_calls[0][0], 123);
+                    assert.equal(goban._handle_move_calls[0][1], 456);
                 });
-                it('doesn\'t call handle_move when touches.length != 1', () => {
+                it('doesn\'t call _handle_move when touches.length != 1', () => {
                     canvas.event_listeners['touchmove'][0](double_touch_event);
-                    assert.equal(goban.handle_move_calls.length, 0);
+                    assert.equal(goban._handle_move_calls.length, 0);
                 });
             });
             describe('touchcancel', () => {
-                it('calls handle_out when touches.length == 1', () => {
+                it('calls _handle_out when touches.length == 1', () => {
                     canvas.event_listeners['touchcancel'][0](single_touch_event);
-                    assert.equal(goban.handle_out_calls, 1);
+                    assert.equal(goban._handle_out_calls, 1);
                 });
-                it('doesn\'t call handle_out when touches.length != 1', () => {
+                it('doesn\'t call _handle_out when touches.length != 1', () => {
                     canvas.event_listeners['touchcancel'][0](double_touch_event);
-                    assert.equal(goban.handle_out_calls, 0);
+                    assert.equal(goban._handle_out_calls, 0);
                 });
             });
         });
@@ -205,79 +205,82 @@ describe('Goban', () => {
     describe('panning', () => {
         it('move after press pans', () => {
             const expected_offset = {x: goban.offset.x + 3, y: goban.offset.y + 4};
-            goban.handle_press(1, 2);
-            goban.handle_move(4, 6);
+            goban._handle_press(1, 2);
+            goban._handle_move(4, 6);
             assert.deepStrictEqual(goban.offset, expected_offset);
         });
         it('move before press doesn\'t pan', () => {
             const initial_offset = {x: goban.offset.x, y: goban.offset.y};
-            goban.handle_move(3,4);
+            goban._handle_move(3,4);
             assert.deepStrictEqual(goban.offset, initial_offset);
         });
         it('move after out after press doesn\'t pan', () => {
             const initial_offset = {x: goban.offset.x, y: goban.offset.y};
-            goban.handle_press(1, 2);
-            goban.handle_out();
-            goban.handle_move(3, 4);
+            goban._handle_press(1, 2);
+            goban._handle_out();
+            goban._handle_move(3, 4);
             assert.deepStrictEqual(goban.offset, initial_offset);
         });
         it('move after release after press doesn\'t pan', () => {
             const initial_offset = {x: goban.offset.x, y: goban.offset.y};
-            goban.handle_press(1, 2);
-            goban.handle_release(1, 2);
-            goban.handle_move(3, 4);
+            goban._handle_press(1, 2);
+            goban._handle_release(1, 2);
+            goban._handle_move(3, 4);
             assert.deepStrictEqual(goban.offset, initial_offset);
         });
     });
 
+    describe('tap', () => {
+        //TODO decide how to test tap vs pan threshold.
+        //TODO decide how to take pan offset into account for these tests.
+        it('tap as white on empty grid point places unconfirmed white stone', () => {
+        });
+        it('tap as black on empty grid point places unconfirmed black stone', () => {
+        });
+
+        it('tap with white team selected on grid point with white stone ignored', () => {
+        });
+        it('tap with black team selected on grid point with white stone ignored', () => {
+        });
+        it('tap with white team selected on grid point with black stone ignored', () => {
+        });
+        it('tap with black team selected on grid point with black stone ignored', () => {
+        });
+
+        it('tap with white team selected on grid point with unconfirmed white stone places white stone, removes unconfirmed stone', () => {
+        });
+        it('tap with black team selected on grid point with unconfirmed white stone places white stone, removes unconfirmed stone', () => {
+        });
+        it('tap with white team selected on grid point with unconfirmed black stone places black stone, removes unconfirmed stone', () => {
+        });
+        it('tap with black team selected on grid point with unconfirmed black stone places black stone, removes unconfirmed stone', () => {
+        });
+    });
 });
 
 
-// Goban
-//     constructor(canvas)
-//     
-//         addsEventListeners: mousedown, mouseup, mouseout, mousemove, touchstart, touchend, touchmove
-//         
-//         EventListener calls on_press: mousedown, touchstart
-// 
-//         EventListener calls on_release: mouseup, touchend
-// 
-//         EventListener calls on_move: mousemove, touchmove
-//            
-// 
-//         touchstart calls on_press when single-touch
-// 
-//         touchstart, touchmove:
-//             stops panning when touchs.length > 1
-// 
-//         touchend calls on_release when single-touch released
-// 
-//         touchend does not call on_release when multi-touch release
-// 
-//     on_press(x, y)
-//     on_move(x, y)
-//     on_release(x, y)
-//     
-//     is_click_release(x, y)
-//         click_press_position
-//         threshold
-// 
-//     on_grid_click_release(x, y)
-//         calls requestAnimationFrame
-//         calls place_unconfirmed_stone when no confirmed stone
-//         confirms unconfirmed stone when clicked on
-// 
-//     to_grid_position(x, y)
+//     handle_grid_point_tap(x, y)
+    //     
+    //     is_click_release(x, y)
+    //         click_press_position
+    //         threshold
+    // 
+    //     on_grid_click_release(x, y)
+    //         calls requestAnimationFrame
+    //         calls place_unconfirmed_stone when no confirmed stone
+    //         confirms unconfirmed stone when clicked on
+    // 
+    //     to_grid_position(x, y)
 // 
 //     draw_grid?
 //     draw_stone?
 //     draw?
 // 
-//     is_point_empty(pos)
-//         
-//     place_unconfirmed_stone(color, pos)
-// 
-//     confirm_stone_placement()
+    //     is_point_empty(pos)
+    //         
+    //     place_unconfirmed_stone(color, pos)
+    // 
+    //     confirm_stone_placement()
 // 
 //     resize()
 //         calls requestAnimationFrame
